a1f18ae016b5780ea27072bf397c8f9c099c1e0a
Gmel2004 <gmelnikob@gmail.com>
2025-02-09 00:21:35 +0500

    initial

---
+++ HomophoneSystem/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextFrom = new System.Windows.Forms.Label();
+            this.ReplaceButton = new System.Windows.Forms.Button();
+            this.ToText = new System.Windows.Forms.RichTextBox();
+            this.FromText = new System.Windows.Forms.RichTextBox();
+            this.TextTo = new System.Windows.Forms.Label();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextFrom
+            // 
+            this.TextFrom.AutoSize = true;
+            this.TextFrom.Location = new System.Drawing.Point(178, 93);
+            this.TextFrom.Name = "TextFrom";
+            this.TextFrom.Size = new System.Drawing.Size(76, 16);
+            this.TextFrom.TabIndex = 1;
+            this.TextFrom.Text = "Original text";
+            // 
+            // ReplaceButton
+            // 
+            this.ReplaceButton.Location = new System.Drawing.Point(362, 90);
+            this.ReplaceButton.Name = "ReplaceButton";
+            this.ReplaceButton.Size = new System.Drawing.Size(75, 23);
+            this.ReplaceButton.TabIndex = 2;
+            this.ReplaceButton.Text = "Replace";
+            this.ReplaceButton.UseVisualStyleBackColor = true;
+            this.ReplaceButton.Click += new System.EventHandler(this.ReplaceButton_Click);
+            // 
+            // ToText
+            // 
+            this.ToText.Enabled = false;
+            this.ToText.Location = new System.Drawing.Point(401, 144);
+            this.ToText.Name = "ToText";
+            this.ToText.Size = new System.Drawing.Size(350, 150);
+            this.ToText.TabIndex = 4;
+            this.ToText.Text = "";
+            // 
+            // FromText
+            // 
+            this.FromText.Location = new System.Drawing.Point(46, 144);
+            this.FromText.Name = "FromText";
+            this.FromText.Size = new System.Drawing.Size(350, 150);
+            this.FromText.TabIndex = 5;
+            this.FromText.Text = "";
+            // 
+            // TextTo
+            // 
+            this.TextTo.AutoSize = true;
+            this.TextTo.Location = new System.Drawing.Point(551, 93);
+            this.TextTo.Name = "TextTo";
+            this.TextTo.Size = new System.Drawing.Size(91, 16);
+            this.TextTo.TabIndex = 6;
+            this.TextTo.Text = "Encrypted text";
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(46, 300);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(705, 60);
+            this.ConvertButton.TabIndex = 7;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.TextTo);
+            this.Controls.Add(this.FromText);
+            this.Controls.Add(this.ToText);
+            this.Controls.Add(this.ReplaceButton);
+            this.Controls.Add(this.TextFrom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TextFrom;
+        private System.Windows.Forms.Button ReplaceButton;
+        private System.Windows.Forms.RichTextBox ToText;
+        private System.Windows.Forms.RichTextBox FromText;
+        private System.Windows.Forms.Label TextTo;
+        private System.Windows.Forms.Button ConvertButton;
+    }
+}
+
---
+++ HomophoneSystem/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string path;
+        private bool isEncryptText = true;
+        Dictionary<char, int> symbolFrequencyes;
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            ToText.Text = isEncryptText ?
+                EncryptText(FromText.Text) :
+                DecryptText(FromText.Text);
+        }
+
+        private void ReplaceButton_Click(object sender, EventArgs e)
+        {
+            isEncryptText = !isEncryptText;
+        }
+
+        private string EncryptText(string text)
+        {
+            StringBuilder result;
+            if (!File.Exists(path) || new FileInfo("file").Length != 0)
+            {
+                GenerateCodes(); // need to returns collection
+                // serealization
+            }
+            
+            //work with collection and text
+            // if symbol in text isn't a key then error
+        }
+
+        private void GenerateCodes()
+        {
+            List<int> possibleCodes = Enumerable.Range(0, 256).ToList();
+            Dictionary<char, List<int>> symbolCodes = new Dictionary<char, List<int>>();
+
+            while (possibleCodes.Count > 0)
+            {
+                foreach (var i in symbolFrequencyes)
+                {
+                    var addedCodes = new List<int>();
+                    var rnd = new Random();
+
+                    for (
+                        int j = 0;
+                        j < possibleCodes.Count / symbolFrequencyes.Count * i.Value;
+                        j++
+                        )
+                    {
+                        var index = rnd.Next(0, possibleCodes.Count - 1);
+                        addedCodes.Add(possibleCodes[index]);
+                        possibleCodes.RemoveAt(index);
+                    }
+
+                    symbolCodes[i.Key] =
+                        symbolCodes[i.Key] == null ?
+                        addedCodes :
+                        symbolCodes[i.Key].Concat(addedCodes).ToList();
+                }
+            }
+        }
+
+        private string DecryptText(string text)
+        {
+            //deserealization
+            //if file isn't exist Error
+        }
+
+        private Dictionary<char, List<int>> ReadJSON(string filename)
+        {
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+            string json = File.ReadAllText(filename);
+            var dict = JsonSerializer.Deserialize<Dictionary<char, List<int>>>(json, options);
+
+            return dict;
+        }
+
+        private void WriteJSON(string filename, Dictionary<char, List<int>> dict)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                IncludeFields = true,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+            string json = JsonSerializer.Serialize(dict, options);
+            File.WriteAllText(filename, json);
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var baseDirectory = Directory.GetParent(Directory.GetCurrentDirectory())
+                .Parent.Parent.FullName;
+            path = $"{baseDirectory}.txt";
+
+            symbolFrequencyes =
+                new Dictionary<char, int>()
+                {
+
+                };
+
+            symbolFrequencyes = (Dictionary<char, int>)symbolFrequencyes.OrderBy(t => t.Value);
+        }
+    }
+}
